832e119943834466fb80f7c099605eda3bc0aee5
Avery Rozar <avery.rozar@trolleyesecurity.com>
2022-02-08 22:17:11 -0500

    remove sonar-scanner-msbuild-5.5.0.42949-net5.0 and add dotnet sdk build and scan

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM ubuntu:20.04
 LABEL maintainer="avery.rozar@trolleyesecurity.com"
 WORKDIR /opt/
 COPY sonar-scanner-4.6.2.2472-linux sonar-scanner-4.6.2.2472-linux/
-COPY sonar-scanner-msbuild-5.5.0.42949-net5.0 sonar-scanner-msbuild-5.5.0.42949-net5.0/
 COPY dependency-check dependency-check/
 COPY scripts scripts/
 ENV TZ=Us/Eastern
@@ -16,7 +15,6 @@ RUN apt update && apt install wget -y && \
         chmod +x /opt/dependency-check/bin/dependency-check.sh &&\
         chmod +x /opt/scripts/run-sonar.sh &&\
         chmod +x /opt/scripts/run-dep-checker.sh &&\
-        chmod +x /opt/sonar-scanner-msbuild-5.5.0.42949-net5.0/sonar-scanner-4.6.2.2472/bin/sonar-scanner &&\
         chmod +x /opt/sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner && \
         dotnet tool install --global dotnet-sonarscanner
 ENV PATH="/opt/scripts:/opt/dependency-check/bin:${PATH}"
--- scripts/run-sonar.sh
+++ scripts/run-sonar.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+git clone --single-branch --branch "$BRANCH" "$REPOURL"
+SCANNER=/opt/sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner
+
 case "$TECH" in
   dotnet)
-    SCANNER=/opt/sonar-scanner-msbuild-5.5.0.42949-net5.0/sonar-scanner-4.6.2.2472/bin/sonar-scanner
-    ;;
-  *)
-    SCANNER=/opt/sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner
+    export PATH="$PATH:/root/.dotnet/tools"
+    dotnet sonarscanner begin /k:"$PROJECTNAME"-"$BRANCH" /d:sonar.login="$SONARLOGIN" /d:sonar.host.url="$SONARHOSTURL"
+    find . -name "*.sln" | while read x; do dotnet build "$x"; done
+    dotnet sonarscanner end /d:sonar.login="$SONARLOGIN"
     ;;
 esac
 
-git clone --single-branch --branch "$BRANCH" "$REPOURL"
 "$SCANNER" -X -Dsonar.host.url="$SONARHOSTURL" -Dsonar.projectKey="$PROJECTNAME"-"$BRANCH" -Dsonar.login="$SONARLOGIN" -Dsonar.projectBaseDir=/opt/"$PROJECTNAME"/ -Dsonar.ce.javaOpts=-Xmx2048m
