480ef9744a8fa0ded138d2c896e9cbccca9a4f9f
Avery Rozar <avery.rozar@trolleyesecurity.com>
2022-02-08 22:53:31 -0500

    add dotnet to project-name

--- scripts/run-sonar.sh
+++ scripts/run-sonar.sh
@@ -6,7 +6,7 @@ SCANNER=/opt/sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner
 case "$TECH" in
   dotnet)
     export PATH="$PATH:/root/.dotnet/tools"
-    dotnet sonarscanner begin /k:"$PROJECTNAME"-"$BRANCH" /d:sonar.login="$SONARLOGIN" /d:sonar.host.url="$SONARHOSTURL"
+    dotnet sonarscanner begin /k:"$PROJECTNAME"-"$BRANCH"-dotnet /d:sonar.login="$SONARLOGIN" /d:sonar.host.url="$SONARHOSTURL"
     find . -name "*.sln" | while read x; do dotnet build "$x"; done
     dotnet sonarscanner end /d:sonar.login="$SONARLOGIN"
     ;;
